c8cf46a5904d3b842b33c4a9b92ffcff1c461380
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-03-31 21:35:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 app.use("/enquiry", enquiryRouter);
-app.use("/stellar", stellarRouter);
+app.use("/api/stellar", stellarRouter);
 // subscribe();
 
 const PORT = process.env.PORT || 5000;
